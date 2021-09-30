cfef5a2e8150961936ff08dcba80fb1081343b07
ola31 <abrahamola80@gmail.com>
2021-10-01 00:28:38 +0900

    (2021-10-01)(nuc)

---
+++ scripts/ex_image.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+
+import sys, time
+import numpy as np
+#from scipy.ndimage import filters
+import cv2
+import roslib
+import rospy
+
+from cv_bridge import CvBridge
+from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import Image
+
+bridge = CvBridge()
+
+image_pub = rospy.Publisher("/output/image_raw/compressed", CompressedImage, queue_size = 1)
+
+def image_listen():
+    rospy.init_node('image_feature', anonymous=True)
+   
+    subscriber = rospy.Subscriber("/camera/color/image_raw",Image, callback, queue_size = 1)
+
+    rospy.spin()
+
+def callback(data_):
+
+    frame = bridge.imgmsg_to_cv2(data_, "bgr8")
+    encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
+    
+        
+    im = cv2.resize(frame, (320, 180))
+    msg = bridge.cv2_to_compressed_imgmsg(im)  
+    image_pub.publish(msg)
+
+       
+
+'''if __name__ == '__main__':
+    main(sys.argv)'''
+
+if __name__ == '__main__':
+    image_listen()
+
